75420c281c7ad39faab23dd392348691d535b38a
Laison Camargo <laison.lopeees@gmail.com>
2026-01-20 22:42:48 -0300

    Comparador de números - Python básico

---
+++ maiornum.py
@@ -0,0 +1,13 @@
+# Código de comparação entre dois valores
+
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor == segundo_valor:
+    print(f'O primeiro valor informado é igual ao segundo valor informado')
+elif  primeiro_valor > segundo_valor:
+    print(f'O valor {primeiro_valor} é maior do que {segundo_valor}')
+else:
+    print(f'O valor {segundo_valor} é maior do que {primeiro_valor}')
+
+print('Fim  código')
